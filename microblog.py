b8574dead11b4ea4f486e111aae801700d918a07
Thiago F C Silveira <thiagofcs@gmail.com>
2022-06-14 06:00:39 -0300

    App Update.

--- microblog.py
+++ microblog.py
@@ -1,5 +1,12 @@
 import os
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processors
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
